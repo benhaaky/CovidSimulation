8070ea4a4c60a307d638f1d6519441144e10e61f
benhaaky <51026756+benhaaky@users.noreply.github.com>
2021-01-31 20:17:52 +0000

    update project files

---
+++ CS310 Project/src/cS310_Project/Susceptible.java
@@ -0,0 +1,13 @@
+package cS310_Project;
+
+import repast.simphony.space.continuous.ContinuousSpace;
+import repast.simphony.space.grid.Grid;
+
+public class Susceptible extends Agent {
+
+	public Susceptible(ContinuousSpace<Object> space, Grid<Object> grid, boolean inf, boolean sus) {
+		super(space, grid, inf, sus);
+		// TODO Auto-generated constructor stub
+	}
+
+}
